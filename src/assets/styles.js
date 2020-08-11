c5cb2a604971dd9525ff199d8bab566dbe8b03e5
Victor Mesquita <mesquitadev@gmail.com>
2020-08-11 10:04:42 -0300

    M - Components - Criando Card em Styled

---
+++ src/assets/styles.js
@@ -0,0 +1,15 @@
+import styled from 'vue-styled-components';
+
+const Card = styled.div`
+    background-color: #042f3a;
+    padding: 16px;
+    border-radius: 8px;
+    box-shadow: 1px solid rgba(0, 0, 0, .15);
+    display: flex;
+    flex-wrap: wrap;
+    width: 100%;
+    max-height: 100%;  
+    padding: 10px auto
+`;
+
+export default Card;
