701a5f6e63bcc49fb828c826c95a9ea40abcfb7b
Victor Mesquita <mesquitadev@gmail.com>
2020-08-11 10:04:25 -0300

    M - Pages, Components - Mudando para estrutura de pages e removendo do main.app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+      <Busca/>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Busca from './pages/Busca'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Busca,
   }
+  
 }
 </script>
 
-<style>
+<style lang="css" scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 10px;
 }
+  
+  
+
+
 </style>
